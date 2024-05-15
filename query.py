84351271ad056966b454e20fe63ba958f892fe26
Julián Gómez <jgomez@atlantiasearch.com>
2024-05-15 09:35:12 -0600

    first commit

---
+++ query.py
@@ -0,0 +1,18 @@
+MAIN_QUERY = """--sql
+SELECT
+  country_name,
+  retailer_name,
+  store_key,
+  master_store_id,
+  store_name,
+  full_date,
+  category_name,
+  product_name,
+  ean_sku_code,
+  product_current_price,
+  product_promo_price
+FROM
+  `prod-shelftia-as.flattening_data.dw_hktn_prices`
+LIMIT
+  1000
+"""
